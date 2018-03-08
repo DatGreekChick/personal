3c652653f248a3f2f7bc3495e96738942f3f6b5e
Eleni A <eleni.arvanitis@me.com>
2018-03-08 13:46:14 -0500

    changed file name for sw

--- public/ifServiceWorker.js
+++ public/ifServiceWorker.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/sw.js')
+    .register('/serviceWorker.js')
     .then(() => console.log('Service Worker Registered'))
     .catch(err => console.error('Error registering service worker:', err))
 }
