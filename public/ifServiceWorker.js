d0ddacc0dfa1d44d7a45974a02265f0e38abd4e9
Eleni A <eleni.arvanitis@me.com>
2018-04-20 09:15:12 -0400

    service worker logic needs to stay inline in index.html

--- public/ifServiceWorker.js
+++
@@ -1,6 +0,0 @@
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/serviceWorker.js')
-    .then(() => console.log('Service Worker Registered'))
-    .catch(err => console.error('Error registering service worker:', err))
-}
