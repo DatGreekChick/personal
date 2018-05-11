cbedf8fa7511d11b9c81400df21944cd3e0508f4
Eleni A <eleni.arvanitis@me.com>
2018-05-11 18:43:37 -0400

    added files to cache, placed font import in index.html, added rels, fixed up some css rules

--- client/components/Work.js
+++ client/components/Work.js
@@ -7,7 +7,7 @@ export default () => {
   windowTrick()
 
   return <div className='work'>
-    <a href='/assets/Eleni-Arvanitis-Resume.pdf' target='_blank'>
+    <a href='/assets/Eleni-Arvanitis-Resume.pdf' target='_blank' rel='noopener'>
       <button className='resume-button'>View Resume</button>
     </a>
     <Expand/>
