76edc09f4e33568c92e4be308802f8c608c9cbab
Eleni A <eleni.arvanitis@me.com>
2018-11-29 22:25:35 -0500

    properly moved these out of presets into plugins

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,26 @@
 const { isHot } = require('./env.config')
 
+const plugins = [
+  // stage 2
+  ['@babel/plugin-proposal-decorators', { legacy: true }],
+  '@babel/plugin-proposal-function-sent',
+  '@babel/plugin-proposal-export-namespace-from',
+  '@babel/plugin-proposal-numeric-separator',
+  '@babel/plugin-proposal-throw-expressions',
+  // stage 3
+  '@babel/plugin-syntax-dynamic-import',
+  '@babel/plugin-syntax-import-meta',
+  ['@babel/plugin-proposal-class-properties', { loose: false }],
+  '@babel/plugin-proposal-json-strings',
+]
+
 const babel = module.exports = env => ({
   loader: 'babel-loader',
   options: {
     presets: [
       ['@babel/preset-env', { modules: false, targets: { uglify: true } }],
       '@babel/preset-react',
-      // stage 2
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
-      '@babel/plugin-proposal-function-sent',
-      '@babel/plugin-proposal-export-namespace-from',
-      '@babel/plugin-proposal-numeric-separator',
-      '@babel/plugin-proposal-throw-expressions',
-      // stage 3
-      '@babel/plugin-syntax-dynamic-import',
-      '@babel/plugin-syntax-import-meta',
-      ['@babel/plugin-proposal-class-properties', { loose: false }],
-      '@babel/plugin-proposal-json-strings',
     ],
-    plugins: isHot(env) && ['react-hot-loader/babel']
+    plugins: isHot(env) ? ['react-hot-loader/babel', ...plugins] : plugins
   }
 })
