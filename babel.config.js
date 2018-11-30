b067dd92f04ecb5a7ebda2358546c8b3f4ef61fc
Eleni A <eleni.arvanitis@me.com>
2018-11-29 21:34:10 -0500

    added stage 2 and stage 3 plugins for babel

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,17 @@ const babel = module.exports = env => ({
     presets: [
       ['@babel/preset-env', { modules: false, targets: { uglify: true } }],
       '@babel/preset-react',
+      // stage 2
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      '@babel/plugin-proposal-function-sent',
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-proposal-numeric-separator',
+      '@babel/plugin-proposal-throw-expressions',
+      // stage 3
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-syntax-import-meta',
+      ['@babel/plugin-proposal-class-properties', { loose: false }],
+      '@babel/plugin-proposal-json-strings',
     ],
     plugins: isHot(env) && ['react-hot-loader/babel']
   }
