59014c5bfbc0e2dde25e8972f167ab2ca7ab332d
Eleni A <eleni.arvanitis@me.com>
2021-03-02 20:44:02 -0500

    Maintenance: Upgrade all packages

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
-import TextLoop from 'react-text-loop'
+import TextTransition, { presets } from 'react-text-transition'
 
 import '~/public/assets/styles/home.css'
 import { me, description} from '~/content/about'
 
-export default () =>
-  <div className='me'>
-    <span>
-      I am <span/>
-      <TextLoop>
-        {
-          me.map(descriptor => {
-            return <span className='carousel-items'
-                         key={descriptor}>
-                {descriptor}
-            </span>
-          })
-        }
-      </TextLoop>
-    </span>
-    <p className='home-description'>{description}</p>
-    <NavLink to='/contact'>
-      <button>Say hi</button>
-    </NavLink>
-  </div>
+export default () => {
+  const [idx, setIdx] = useState(0)
+
+  useEffect(() => {
+    const intervalId = setInterval(() =>
+        setIdx(idx => ++idx),
+      3000 // every 3 seconds
+    );
+    return () => clearTimeout(intervalId);
+  }, [])
+
+    return (
+      <div className='me'>
+        <span>
+          I am <span/>
+          <TextTransition
+            className='carousel-items'
+            direction='up'
+            inline={true}
+            text={me[idx % me.length]}
+            springConfig={presets.wobbly}
+          />
+        </span>
+        <p className='home-description'>{description}</p>
+        <NavLink to='/contact'>
+          <button>Say hi</button>
+        </NavLink>
+      </div>
+    )
+}
