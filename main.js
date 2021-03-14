0117f2586cec6661f9f0ad2a47eabc0802e4532b
Eleni A <eleni.arvanitis@me.com>
2021-03-14 17:42:18 -0400

    Add toast for form submissions

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
+import { ToastProvider } from 'react-toast-notifications'
 
 import App from '~/client/App'
 
 function main() {
   render(
     <AppContainer>
-      <App />
+      <ToastProvider autoDismissTimeout={3000} autoDismiss={true}>
+        <App />
+      </ToastProvider>
     </AppContainer>,
     document.getElementById('main')
   )
