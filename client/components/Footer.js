5d949466d3cdeb793dcfe2ebb1ae9602d91fe0dc
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2021-03-20 13:59:57 -0400

    Styles: Make styles more responsive across devices (#78)

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -7,7 +7,7 @@ import '~/public/assets/styles/footer.css'
 export default () => (
   <div className='footer' role='contentinfo'>
     <span className='copyright'>
-      &copy; {`${new Date().getFullYear()} Eleni Arvanitis Konior`}
+      &copy; {`${new Date().getFullYear()} Eleni Konior`}
     </span>
     <div>
       {socialLinks.map(link => (
