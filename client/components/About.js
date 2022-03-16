a3d436ae4cacb468e3e293eea7aab03991e607a0
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2022-03-16 13:33:55 -0400

    Enhancement: Pull text into relevant Home, About components (#119)
    * Enhancement: Pull text into relevant Home, About components
    
    * Run prettier

--- client/components/About.js
+++ client/components/About.js
@@ -2,16 +2,35 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 import Button from '~/client/components/Button'
-
-import { aboutHeader, aboutText1, aboutText2 } from '~/content/about'
-
 import { About, AboutHeader, AboutText } from '~/client/styles/about'
 
 export default () => (
   <About>
-    <AboutHeader>{aboutHeader}</AboutHeader>
-    <AboutText>{aboutText1}</AboutText>
-    <AboutText>{aboutText2}</AboutText>
+    <AboutHeader>Creating useful and engaging software</AboutHeader>
+    <AboutText>
+      <p>
+        Every day I get to ride the insatiable roller coaster that is software
+        engineering! I've built extensible Python pip packages, streamlined
+        aggregation engines in C++, created valuable HTTP REST APIs using Flask,
+        designed beautiful React frontends tracking diversity initiatives. I'm
+        an engaging speaker, produce minimalist code, write killer
+        documentation, and test everything. I absolutely love to learn, get a
+        thrill when teaching others, and really dig pair programming.
+      </p>
+      <p>
+        I'm advanced in Python, JavaScript/TypeScript, Node, C++, Webpack, SQL
+        and NoSQL databases, Firebase, React, HTML, and CSS. I also have some
+        experience with AWS (Lambda), Java, Rust, and robotics hardware from
+        independent projects. I thrive in environments that constantly push me
+        to learn and want to learn more programming languages, including Swift
+        and Go. For more of my experience, check out my work below!
+      </p>
+      <p>
+        When I'm not on the job, I love to venture outdoors either via biking,
+        kayaking, or hiking, taking photos while traveling, catching a game of
+        soccer, or strumming my bass guitar.
+      </p>
+    </AboutText>
     <NavLink exact to='/work'>
       <Button text='See my work' />
     </NavLink>
