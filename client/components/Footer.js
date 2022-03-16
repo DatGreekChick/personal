a36651256360ef7e9cdf7fb14ad62e4c34b26fd2
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2022-03-16 11:58:46 -0400

    Enhancement: Commit previously git-ignored files for CI (#117)

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons/faGitHubAlt'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin'
+import { faMediumM } from '@fortawesome/free-brands-svg-icons/faMediumM'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope'
 
 import { Footer, Copyright, Icon } from '~/client/styles/footer'
 
-import { socialLinks } from '~/content/secrets'
+const socialLinks = [
+  ['https://github.com/datgreekchick', faGithubAlt, '#6e5494'],
+  ['https://linkedin.com/in/eleniarvanitis', faLinkedin, '#0077b5'],
+  ['https://datgreekchick.medium.com', faMediumM, '#00ab6c'],
+  ['https://twitter.com/datgreekchick', faTwitter, '#1DA1F2'],
+  ['mailto:eleni.arvanitis@me.com', faEnvelope, '#fbbc05'],
+]
 
 export default () => (
   <Footer role='contentinfo'>
