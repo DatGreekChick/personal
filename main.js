2729e9814413c3a56554c4758a4d09925aefa7fd
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2022-03-17 15:51:26 -0400

    chore(refactor): use updated react-hot-loader code

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import { render } from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
 import { ToastProvider } from 'react-toast-notifications'
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 
@@ -10,18 +9,13 @@ import App from '~/client/App'
 
 function main() {
   render(
-    <AppContainer>
-      <GoogleReCaptchaProvider reCaptchaKey={process.env.RECAPTCHA_SITE_KEY}>
-        <ToastProvider autoDismissTimeout={3000} autoDismiss={true}>
-          <App />
-        </ToastProvider>
-      </GoogleReCaptchaProvider>
-    </AppContainer>,
+    <GoogleReCaptchaProvider reCaptchaKey={process.env.RECAPTCHA_SITE_KEY}>
+      <ToastProvider autoDismissTimeout={3000} autoDismiss={true}>
+        <App />
+      </ToastProvider>
+    </GoogleReCaptchaProvider>,
     document.getElementById('main')
   )
 }
 
 main()
-
-// eslint-disable-next-line no-undef
-module.hot && module.hot.accept('~/client/App', main)
