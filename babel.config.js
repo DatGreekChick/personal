2427247d6e4c21622fa037ab2941e5573d1c8061
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2022-03-17 12:13:14 -0400

    Refactor: Update babel to leverage ES2022 (#128)

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,6 @@
 const { isHot } = require('./env.config')
 
-const plugins = [
-  // stage 2
-  ['@babel/plugin-proposal-decorators', { legacy: true }],
-  '@babel/plugin-proposal-function-sent',
-  '@babel/plugin-proposal-export-namespace-from',
-  '@babel/plugin-proposal-numeric-separator',
-  '@babel/plugin-proposal-throw-expressions',
-  // stage 3
-  '@babel/plugin-syntax-dynamic-import',
-  '@babel/plugin-syntax-import-meta',
-  ['@babel/plugin-proposal-class-properties', { loose: false }],
-  '@babel/plugin-proposal-json-strings',
-  // styled-components
-  'babel-plugin-styled-components',
-]
+const plugins = ['babel-plugin-styled-components']
 
 const babel = (module.exports = {
   presets: [
