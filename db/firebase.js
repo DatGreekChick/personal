6104ca9a273da283e1076d7ab51dcede8cd839f0
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2022-03-17 15:28:30 -0400

    chore(update): refactor firebase code to use modular sdk rather than compat (#134)

--- db/firebase.js
+++ db/firebase.js
@@ -1,9 +1,9 @@
-/* eslint no-undef: 0 */
+/* eslint no-undef: 0 no-unused-vars: 0 */
 
-import firebase from 'firebase/compat/app'
+import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
-firebase.initializeApp({
+const fire = initializeApp({
   apiKey: process.env.FIREBASE_API_KEY,
   databaseURL: 'https://elenicodes.firebaseio.com',
   projectId: 'elenicodes',
