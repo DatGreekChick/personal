18dd0e6f5fc184cbacdef1a5f7f38ec2e99bc219
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2022-06-13 19:35:36 -0400

    refactor: normalize header styles

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -3,19 +3,15 @@ import Form from '~/client/components/Form'
 
 import windowTrick from '~/client/window'
 
-import {
-  StyledContact,
-  ContactForm,
-  H2,
-  Disclaimer,
-} from '~/client/styles/contact'
+import { StyledContact, ContactForm, Disclaimer } from '~/client/styles/contact'
+import { Header } from '~/client/styles'
 
 const Contact = () => {
   windowTrick()
 
   return (
     <StyledContact>
-      <H2>Let&apos;s Work Together!</H2>
+      <Header fontSize='60'>Let&apos;s Work Together!</Header>
       <ContactForm>
         <span>
           <Form />
