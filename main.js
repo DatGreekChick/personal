5cdd57f440ad6b12ae4ff6894ab49b1e63da7707
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2022-06-20 09:07:37 -0400

    feat/refactor: use react-hot-toast for simplification

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
-import { ToastProvider } from 'react-toast-notifications'
 import { ThemeProvider } from 'styled-components'
 
 import App from '~/client/App'
@@ -14,12 +13,10 @@ function main() {
   root.render(
     // eslint-disable-next-line no-undef
     <GoogleReCaptchaProvider reCaptchaKey={process.env.RECAPTCHA_SITE_KEY}>
-      <ToastProvider autoDismissTimeout={3000} autoDismiss={true}>
-        <ThemeProvider theme={{ fontFamily: 'Open Sans, sans-serif' }}>
-          <GlobalStyle />
-          <App />
-        </ThemeProvider>
-      </ToastProvider>
+      <ThemeProvider theme={{ fontFamily: 'Open Sans, sans-serif' }}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </GoogleReCaptchaProvider>
   )
 }
