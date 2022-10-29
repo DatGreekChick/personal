b8f37ebc1edf6ff7a7e49bfe4e8f372d2070cb82
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2022-10-29 15:06:31 -0400

    refactor & [fix]: move projects into `Work`
    Extracting out a `Project` component was proving a bit more difficult
    than it should have been. I've decided instead to beef up the `Work`
    component a bit more in order to reduce levels of indirection and
    increase clarity behind what actually makes up the `Work` component.

--- client/hooks/useExpansion.js
+++ client/hooks/useExpansion.js
@@ -4,25 +4,28 @@ const useExpansion = () => {
   const [isExpanded, setIsExpanded] = useState(false)
   const [expandedItem, setExpandedItem] = useState('')
 
-  const toggle = useCallback(evt => {
-    const item = evt.target.innerText
+  const toggle = useCallback(
+    evt => {
+      const item = evt.target.innerText
 
-    if (!expandedItem) {
-      setExpandedItem(item)
-    } else if (expandedItem && item !== expandedItem) {
-      // if there's an expanded item, but a different item was clicked, then
-      // hide the current item, set the new item, then expand that new item
-      setIsExpanded(false)
-      setExpandedItem(item)
-      setIsExpanded(true)
-    }
+      if (!expandedItem) {
+        setExpandedItem(item)
+      } else if (expandedItem && item !== expandedItem) {
+        // if there's an expanded item, but a different item was clicked, then
+        // hide the current item, set the new item, then expand that new item
+        setIsExpanded(false)
+        setExpandedItem(item)
+        setIsExpanded(true)
+      }
 
-    // if there isn't already an expanded item, or the same item was clicked,
-    // then set isExpanded to the opposite value
-    if (!expandedItem || expandedItem === item) {
-      setIsExpanded(!isExpanded)
-    }
-  }, [])
+      // if there isn't already an expanded item, or the same item was clicked,
+      // then set isExpanded to the opposite value
+      if (!expandedItem || expandedItem === item) {
+        setIsExpanded(!isExpanded)
+      }
+    },
+    [isExpanded, expandedItem]
+  )
 
   return { toggle, isExpanded, expandedItem }
 }
