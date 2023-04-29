f88d49956b14f40d3cbbf2c7997bd23c526ae4b7
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2023-04-29 12:25:02 -0400

    refactor: move footer styles to Footer component

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { brands, solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-import { StyledFooter, Copyright, Icon } from '~/client/styles/footer'
-
-const socialLinks = [
+const SOCIAL_LINKS = [
   ['https://github.com/datgreekchick', brands('github'), '#6e5494'],
   ['https://linkedin.com/in/eleniarvanitis', brands('linkedin'), '#0077b5'],
   ['https://datgreekchick.medium.com', brands('medium'), '#00ab6c'],
@@ -12,11 +11,34 @@ const socialLinks = [
   ['mailto:eleni.arvanitis@me.com', solid('at'), '#fbbc05'],
 ]
 
+const StyledFooter = styled.footer`
+  position: fixed;
+  margin-bottom: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 2%;
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 1);
+  z-index: 2;
+`
+
+const Icon = styled.a`
+  padding-left: 12px;
+
+  :hover {
+    color: ${props => props.color};
+  }
+`
+
 const Footer = () => (
   <StyledFooter role='contentinfo'>
-    <Copyright>&copy; {`${new Date().getFullYear()} Eleni Konior`}</Copyright>
+    <span style={{ color: '#858686' }}>
+      &copy; {`${new Date().getFullYear()} Eleni Konior`}
+    </span>
     <div>
-      {socialLinks.map(link => (
+      {SOCIAL_LINKS.map(link => (
         <Icon
           key={link[0]}
           href={link[0]}
