8862b24085c337954972984e21567a41b45a28c5
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2023-04-29 12:37:47 -0400

    refactor: move nav styles to Navbar, Hamburger components

--- client/components/Hamburger.jsx
+++ client/components/Hamburger.jsx
@@ -1,7 +1,55 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
 import { Sling } from 'hamburger-react'
 
-import { MenuItem, MenuList, StyledNavLink } from '~/client/styles/nav'
+const MenuList = styled.ul`
+  list-style: none;
+  flex-direction: column;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  position: fixed;
+  top: 10%;
+  z-index: 1;
+  left: 0;
+  width: 100%;
+  height: calc(100% - 4rem);
+  background-color: rgba(0, 0, 0, 1);
+`
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  position: relative;
+  color: ghostwhite;
+
+  :before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: #e0bf9f;
+    visibility: hidden;
+    transform: scaleX(0);
+    -webkit-transform: scaleX(0);
+    transition: all 0.3s ease-in-out 0s;
+    -webkit-transition: all 0.3s ease-in-out 0s;
+  }
+
+  :hover:before {
+    color: #e0bf9f;
+    visibility: visible;
+    -webkit-transform: scaleX(1);
+    transform: scaleX(1);
+  }
+
+  :active {
+    border-bottom: 1.5px solid #e0bf9f;
+    color: inherit;
+  }
+`
 
 const LINKS = ['Home', 'About', 'Work', 'Articles', 'Contact']
 
@@ -21,14 +69,14 @@ const Hamburger = () => {
       {open && (
         <MenuList>
           {LINKS.map(link => (
-            <MenuItem key={link}>
+            <li key={link} style={{ margin: 0, padding: '1%' }}>
               <StyledNavLink
                 to={link === 'Home' ? '/' : `/${link.toLowerCase()}`}
                 onClick={toggleMenu}
               >
                 {link}
               </StyledNavLink>
-            </MenuItem>
+            </li>
           ))}
         </MenuList>
       )}
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
 import { NavLink, Outlet } from 'react-router-dom'
 
 import Hamburger from '~/client/components/Hamburger'
 
-import { Nav, Logo } from '~/client/styles/nav'
+const Nav = styled.nav`
+  top: 0;
+  font-size: 23px;
+  font-weight: 800;
+  color: ghostwhite;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 2%;
+  padding: 1rem;
+  vertical-align: text-top;
+`
+
+const Logo = styled.img`
+  display: flex;
+  align-items: center;
+  min-width: 15%;
+  max-height: 30px;
+`
 
 const Navbar = () => (
   <>
