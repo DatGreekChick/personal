191fe501f26b44943d368834f71a2619b2d0bf33
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2023-06-17 08:21:32 -0400

    chore(extract): move styles/home into `<Home />` component (#174)
    * chore(extract): move styles/home into `<Home />` component
    
    This commit is a continuation of placing `styled-components` that
    are specific to a React component into the component source file
    itself. This is for ease of refactoring if necessary and to simplify
    the codebase.
    
    * Remove exports

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,9 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
-import { presets } from 'react-text-transition'
+import TextTransition, { presets } from 'react-text-transition'
+import styled from 'styled-components'
 
 import Button from '~/client/components/Button'
-import { Me, Carousel, Description } from '~/client/styles/home'
+
+const Me = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding-top: 5%;
+  font-size: 18pt;
+  flex-flow: column nowrap;
+  margin: 0 auto;
+  width: 80%;
+
+  @media (min-width: 0px) and (max-width: 320px) {
+    font-size: 12pt;
+  }
+`
+
+const Carousel = styled(TextTransition)`
+  font-weight: 600;
+  color: #e0bf9f;
+`
+
+const Description = styled.p`
+  font-size: 14pt;
+  font-weight: 300;
+  padding-top: 2%;
+  padding-bottom: 2%;
+
+  @media (min-width: 0px) and (max-width: 319px) {
+    font-size: 12pt;
+  }
+
+  @media (min-width: 320px) and (max-width: 767px) {
+    font-size: 13pt;
+  }
+`
 
 const ME = [
   'a software engineer 👩🏻‍💻',
