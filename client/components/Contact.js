93c6f1b9c04d0481444eb640cc6dc5af1d5681c7
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2023-06-24 12:22:41 -0400

    [fix] chrome dev tools errors (#176)

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -1,17 +1,22 @@
 import React from 'react'
 
 import Form from '~/client/components/Form'
+import Link from '~/client/components/Link'
 
 import { Header } from '~/client/styles'
 import { Disclaimer, StyledContact } from '~/client/styles/contact'
+
+const GOOGLE_PRIVACY_POLICY = 'https://policies.google.com/privacy?hl=en-US'
+const GOOGLE_TERMS_OF_SERVICE = 'https://policies.google.com/terms?hl=en-US'
 
 const Contact = () => (
   <StyledContact>
     <Header fontSize='45'>Let&apos;s Work Together!</Header>
     <Form />
     <Disclaimer>
-      This site is protected by reCAPTCHA and the Google Privacy Policy and
-      Terms of Service apply.
+      This site is protected by reCAPTCHA and the Google{' '}
+      <Link href={GOOGLE_PRIVACY_POLICY}>Privacy Policy</Link> and{' '}
+      <Link href={GOOGLE_TERMS_OF_SERVICE}>Terms of Service</Link> apply.
     </Disclaimer>
   </StyledContact>
 )
