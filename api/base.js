9b9f34e06bc43e3c492345c151953412ed63dab7
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2024-12-30 15:08:40 -0500

    Use baseQuery instead of baseURL

--- api/base.js
+++ api/base.js
@@ -1,6 +1,6 @@
 import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const api = createApi({
-  baseURL: fakeBaseQuery(),
+  baseQuery: fakeBaseQuery(),
   endpoints: () => ({}),
 })
