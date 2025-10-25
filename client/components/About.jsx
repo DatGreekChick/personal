6f8aff1b5e9bd4b3359310cb70f4c5e1fe082c16
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2025-10-25 15:31:10 -0400

    feat: update resume! (#302)

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -16,19 +16,20 @@ export const About = () => (
       <p>
         Every day I get to ride the insatiable roller coaster that is software
         engineering! I&apos;ve built extensible Python pip packages, streamlined
-        aggregation engines in C++, created valuable HTTP REST APIs using Flask,
-        designed beautiful React frontends with millions of monthly hits.
-        I&apos;m an engaging speaker, produce minimalist code, write killer
-        documentation, and test everything. I absolutely love to learn, get a
-        thrill when teaching others, and really dig pair programming.
+        aggregation engines in C++, created valuable HTTP REST APIs using Flask
+        or Rails, designed beautiful React frontends with millions of monthly
+        hits. I&apos;m an engaging speaker, produce minimalist code, design
+        simple but powerful systems, write killer documentation, and test
+        everything. I absolutely love to learn, get a thrill when teaching
+        others, and really dig pair programming.
       </p>
       <p>
-        I&apos;m advanced in Python, JavaScript/TypeScript, Node, C++, Webpack,
-        SQL and NoSQL databases, Firebase, React, HTML, and CSS. I also have
-        some experience with AWS (Lambda), Java, Rust, and robotics hardware
-        from independent projects. I thrive in environments that constantly push
-        me to learn and want to learn more programming languages, including
-        Swift and Go. For more of my experience, check out my work below!
+        I&apos;m advanced in Python, TypeScript, Ruby/Rails, C++, Webpack, SQL
+        and NoSQL databases, Firebase, React, HTML, and CSS. I also have
+        experience with AWS (Lambda, S3), Docker, Grafana, and Splunk. I thrive
+        in environments that constantly push me to learn and want to learn more
+        programming languages, including Swift and Go. For more of my
+        experience, check out my work below!
       </p>
       <p>
         When I&apos;m not on the job, I love to venture outdoors either via
