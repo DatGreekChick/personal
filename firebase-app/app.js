e64acb096c1c165f54389ef1a9a8a346967b6add
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2026-04-18 09:30:31 -0400

    fix: correct manifest dir to ltr; remove unused Firebase databaseURL (#418)

--- firebase-app/app.js
+++ firebase-app/app.js
@@ -16,7 +16,6 @@ const recaptchaToken =
 
 export const app = initializeApp({
   apiKey: process.env.FIREBASE_API_KEY,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   appId: process.env.FIREBASE_APP_ID,
 })
